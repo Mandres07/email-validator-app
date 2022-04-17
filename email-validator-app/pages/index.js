60cbce048772ddcf49841ce0d5f34b75b3b40e33
Mario Morales <mario.morales@devsu.com>
2022-04-17 09:46:42 -0500

    updated readme

--- email-validator-app/pages/index.js
+++ email-validator-app/pages/index.js
@@ -21,7 +21,7 @@ const Home = () => {
    return (
       <div className="flex flex-col items-center relative min-h-screen">
          <h2 className="font-raleway font-bold text-6xl text-primary pt-20 pb-6 md:text-3xl">
-            Email <span className="text-secondary">Validator</span> App
+            <span className='text-primary'>Mandres07</span> Email <span className="text-secondary">Validator</span> App
          </h2>
          <h3 className="text-lightGrey text-2xl font-raleway font-bold uppercase tracking-wide mb-12 md:text-base md:px-4 md:text-center">
             Check if an email address exists or not
